13f44912682fd4efc95efbaf317ec8a754e257b2
nicolo fossati <nicofossa01@gmail.com>
2023-03-06 11:32:26 +0100

    copy-paste setup

--- js/main.js
+++ js/main.js
@@ -1,30 +1,70 @@
-/*
-Consegna
-L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-*/
-
 const containerDom = document.querySelector('#main-container'); 
 const playButtonDom = document.querySelector('#play'); 
+const difficultyDom = document.querySelector('#difficulty'); 
 
 playButtonDom.addEventListener('click', function(){
-    for(let i=1; i<101; i++){
-        const squareDom = document.createElement('div');
-        squareDom.classList.add('square');
+    let levelValue = difficultyDom.value;
+    if(levelValue == "easy"){
+        containerDom.innerHTML = "";
+        for(let i=1; i<101; i++){
+            const squareDom = document.createElement('div');
+            squareDom.classList.add('square');
+            
+            const spanDom = document.createElement('span');
+            spanDom.innerHTML = i;
+            spanDom.classList.add('center');
         
-        const spanDom = document.createElement('span');
-        spanDom.innerHTML = i;
-        spanDom.classList.add('center');
-    
-        squareDom.addEventListener('click', function(){
-            this.classList.add('onclick');
-            console.log(i);
-            spanDom.innerHTML = "";
-        });
-    
-        containerDom.append(squareDom);
-        squareDom.append(spanDom);
+            squareDom.addEventListener('click', function(){
+                this.classList.add('onclick');
+                console.log(i);
+                spanDom.innerHTML = "";
+            });
+        
+            containerDom.append(squareDom);
+            squareDom.append(spanDom);
+        }
+    } else if(levelValue == "hard"){
+        containerDom.innerHTML = "";
+        for(let i=1; i<82; i++){
+            const squareDom = document.createElement('div');
+            squareDom.classList.add('square');
+            squareDom.classList.add('square-hard');
+            
+            const spanDom = document.createElement('span');
+            spanDom.innerHTML = i;
+            spanDom.classList.add('center');
+        
+            squareDom.addEventListener('click', function(){
+                this.classList.add('onclick');
+                console.log(i);
+                spanDom.innerHTML = "";
+            });
+        
+            containerDom.append(squareDom);
+            squareDom.append(spanDom);
+        }
+    } else if(levelValue == "crazy"){
+        containerDom.innerHTML = "";
+        for(let i=1; i<50; i++){
+            const squareDom = document.createElement('div');
+            squareDom.classList.add('square');
+            squareDom.classList.add('square-crazy');
+            
+            const spanDom = document.createElement('span');
+            spanDom.innerHTML = i;
+            spanDom.classList.add('center');
+        
+            squareDom.addEventListener('click', function(){
+                this.classList.add('onclick');
+                console.log(i);
+                spanDom.innerHTML = "";
+            });
+        
+            containerDom.append(squareDom);
+            squareDom.append(spanDom);
+        }
+    } else {
+        
     }
 });
+
